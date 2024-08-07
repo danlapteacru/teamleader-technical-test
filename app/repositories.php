c44babaf1acdcba5bde6b60474f02053e9cf9278
Dan Lapteacru <danlapteacru@gmail.com>
2024-08-07 17:39:39 +0300

    Added  infrastructure persistence

--- app/repositories.php
+++ app/repositories.php
@@ -3,8 +3,10 @@
 declare(strict_types=1);
 
 use App\Domain\Entities\Category\CategoryRepository;
+use App\Domain\Entities\Customer\CustomerRepository;
 use App\Domain\Entities\Product\ProductRepository;
 use App\Infrastructure\Persistence\Category\InMemoryCategoryRepository;
+use App\Infrastructure\Persistence\Customer\InMemoryCustomerRepository;
 use App\Infrastructure\Persistence\Product\InMemoryProductRepository;
 use DI\ContainerBuilder;
 
@@ -13,6 +15,7 @@ use function DI\autowire;
 return function (ContainerBuilder $containerBuilder) {
     $containerBuilder->addDefinitions([
         CategoryRepository::class => autowire(InMemoryCategoryRepository::class),
+        CustomerRepository::class => autowire(InMemoryCustomerRepository::class),
         ProductRepository::class => autowire(InMemoryProductRepository::class),
     ]);
 };
